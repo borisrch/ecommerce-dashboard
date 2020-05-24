3575ddb638e796292f2c228448b5b5dc4c8a27e3
Boris Chan <borisc1996@gmail.com>
2020-05-24 16:43:16 -0400

    remove unused imports

--- src/main/java/com/boris/ecommercedashboard/security/JWTAuthenticationFilter.java
+++ src/main/java/com/boris/ecommercedashboard/security/JWTAuthenticationFilter.java
@@ -3,6 +3,8 @@ package com.boris.ecommercedashboard.security;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -15,6 +17,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
