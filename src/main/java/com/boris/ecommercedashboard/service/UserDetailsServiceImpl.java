58d4b91648e29e07478c0b0de5d26a5a14588f48
Boris Chan <borisc1996@gmail.com>
2020-05-24 15:20:30 -0400

    pass authorities as mapped from ApplicationUser/Roles to User

--- src/main/java/com/boris/ecommercedashboard/service/UserDetailsServiceImpl.java
+++ src/main/java/com/boris/ecommercedashboard/service/UserDetailsServiceImpl.java
@@ -1,5 +1,8 @@
 package com.boris.ecommercedashboard.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -7,18 +10,23 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.boris.ecommercedashboard.repository.ApplicationUserRepository;
+import com.boris.ecommercedashboard.security.JWTAuthenticationFilter;
 import com.boris.ecommercedashboard.user.ApplicationUser;
-import static java.util.Collections.emptyList;
+import com.boris.ecommercedashboard.user.Role;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
 	private ApplicationUserRepository applicationUserRepository;
 	
+	private static final Logger log = LoggerFactory.getLogger(UserDetailsServiceImpl.class);
+	
 	public UserDetailsServiceImpl(ApplicationUserRepository applicationUserRepository) {
 		this.applicationUserRepository = applicationUserRepository;
 	}
-
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -26,7 +34,24 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 		if (applicationUser == null) {
 			throw new UsernameNotFoundException(username);
 		}
-		return new User(applicationUser.getUsername(), applicationUser.getPassword(), emptyList());
+		
+		return new User(applicationUser.getUsername(), applicationUser.getPassword(), getAuthority(applicationUser));
+	}
+	
+	private Set<SimpleGrantedAuthority> getAuthority(ApplicationUser user) {
+		Set<SimpleGrantedAuthority> authorities = new HashSet<>();
+		user.getRoles().forEach(role -> {
+			log.info(role.getName());
+			
+			SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority("ROLE_" + role.getName());
+			log.info(simpleGrantedAuthority.toString());
+			
+			authorities.add(simpleGrantedAuthority);
+		});
+		
+		log.info("HashSet: " + authorities.toString());
+		
+		return authorities;
 	}
 
 }
