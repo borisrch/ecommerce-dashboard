f32e6ecad6c9c185e18a2615655f49666a4d8125
Boris Chan <borisc1996@gmail.com>
2020-05-24 14:48:33 -0400

    add Role class, and many-to-many relationship with ApplicationUsers to Roles.

--- src/main/java/com/boris/ecommercedashboard/user/ApplicationUser.java
+++ src/main/java/com/boris/ecommercedashboard/user/ApplicationUser.java
@@ -1,9 +1,16 @@
 package com.boris.ecommercedashboard.user;
 
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.JoinColumn;
 
 @Entity
 public class ApplicationUser {
@@ -14,6 +21,29 @@ public class ApplicationUser {
 	private String username;
 	private String password;
 	
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "USER_ROLES", joinColumns = {
+            @JoinColumn(name = "USER_ID") }, inverseJoinColumns = {
+            @JoinColumn(name = "ROLE_ID") })
+    private Set<Role> roles;
+
+	
+	public ApplicationUser() {
+		super();
+	}
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
 	public long getId() {
 		return id;
 	}
